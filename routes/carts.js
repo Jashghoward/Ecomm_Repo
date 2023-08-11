fcf828ab1652f0cc778884397bc0376874603852
Josh Howard <jashghoward@icloud.com>
2023-08-11 23:23:39 +0200

    carts.js update

--- routes/carts.js
+++ routes/carts.js
@@ -15,20 +15,14 @@ router.post('/cart/products', async  (req, res) => {
     cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
   } else {
-    // We have a cart! Lets get it from the repository
+
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
   console.log (cart);
-  // Either increment quantity for existing product
-  // OR add new product to items array
 
   res.send('Product added to cart');
 });
-
-// Receive a GET request to show all items in cart
-
-// Receive a post request to delete an item from a cart
 
 module.exports = router;
 
