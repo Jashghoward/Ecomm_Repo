fe89c106b58535145131f6e7ae2c92013d299728
Josh Howard <jashghoward@icloud.com>
2023-08-26 23:51:37 +0200

    index.js update

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const productsRouter = require('./routes/products');
 
 const app = express();
 
-
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
